c8642adef9a119875e796c6e6d9b844105dbeb22
wcallaghan <90016258+wcallaghan@users.noreply.github.com>
2021-10-13 17:01:45 -0400

    Add files via upload

---
+++ db1.js
@@ -0,0 +1,16 @@
+//functions to display the answer on click
+function answer1() {
+    document.getElementById('answer1').innerHTML = "Javascript was created in 1995 by the Netscape corperation as a scripting language for their web browser Netscape Navigator which was one of the early web browsers with a GUI. Javascript was born from the collaboration between Java programming language and Scheme."
+}
+
+function answer2() {
+    document.getElementById('answer2').innerHTML = "Fire and Ice by: Robert Frost<br>Some say the world will end in fire,<br>Some say in ice.<br>From what I've tasted of desire<br>I hold with those who favor fire.<br>But if it had to perish twice,<br>I think I know enough of hate<br>To know that for destruction ice<br>Is also great<br>And would suffice.<br>"
+}
+
+function answer3() {
+    document.getElementById('answer3').innerHTML = "I learned the ECMA script is the structure and syntax for JavaScript. I learned the difference between DOM and BOM in that BOM is related to the browser specifically. I learned what concatentation meant, to link together.  "
+}
+
+function answer4() {
+    document.getElementById('answer4').innerHTML = "Though they stem from the same family there are differences between the C-family programming languages. Notably C is not object-oriented it is procedureal while C++ is both. C# was inspired by C but is used mostly to develope programs on Microsoft's .Net framework. Javascript differs from these languages by being interperted instead of compiled and executed."
+}
